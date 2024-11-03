9e6345936f4f0a87f40fd29699305621fa9b2cbc
Bakhtyar Ansari <bakhtyaransari1@gmail.com>
2024-11-03 18:32:11 +0530

    wow

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -98,9 +98,11 @@ export default function App() {
     0
   );
   useEffect(function () {
-    fetch("http://localhost:8000/questions")
+    fetch("/react-quiz/questions.json")
       .then((res) => res.json())
-      .then((data) => dispatch({ type: "dataRecieved", payload: data }))
+      .then((data) =>
+        dispatch({ type: "dataRecieved", payload: data.questions })
+      )
       .catch((err) => {
         dispatch({ type: "dataFailed" });
         console.error("Error", err);
